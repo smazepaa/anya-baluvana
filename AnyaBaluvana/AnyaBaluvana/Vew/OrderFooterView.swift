3c00a8378d82adaf6653ec3c6291934a0de9e706
smazepaa <sof.mazepa@gmail.com>
2024-12-19 09:02:14 +0200

    fixed map

--- AnyaBaluvana/AnyaBaluvana/Vew/OrderFooterView.swift
+++ AnyaBaluvana/AnyaBaluvana/Vew/OrderFooterView.swift
@@ -7,6 +7,7 @@ final class OrderFooterView: UIView {
     let mapView = MKMapView()
     let addressTextField = UITextField()
     let confirmButton = UIButton(type: .system)
+    private let geocoder = CLGeocoder()
 
     var onConfirmTapped: (() -> Void)?
 
@@ -26,6 +27,12 @@ final class OrderFooterView: UIView {
         addressTextField.placeholder = "Enter delivery address manually"
         addressTextField.borderStyle = .roundedRect
         
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleMapTap(_:)))
+        mapView.addGestureRecognizer(longPressGesture)
+        mapView.layer.cornerRadius = 8
+        mapView.delegate = self
+        mapView.delegate = self
+
         confirmButton.setTitle("Confirm Order", for: .normal)
         confirmButton.titleLabel?.font = .boldSystemFont(ofSize: 18)
         confirmButton.backgroundColor = .systemPink
@@ -65,10 +72,63 @@ final class OrderFooterView: UIView {
     @objc private func confirmTapped() {
         onConfirmTapped?()
     }
+    
+    @objc private func handleMapTap(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            let location = gesture.location(in: mapView)
+            let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+            mapView.removeAnnotations(mapView.annotations)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "Selected Location"
+            mapView.addAnnotation(annotation)
+            reverseGeocode(coordinate: coordinate)
+        }
+    }
+    
+    private func reverseGeocode(coordinate: CLLocationCoordinate2D) {
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        geocoder.reverseGeocodeLocation(location) { [weak self] (placemarks, error) in
+            if let error = error {
+                print("Reverse geocoding failed: \(error.localizedDescription)")
+                self?.addressTextField.text = "Unable to find address"
+                return
+            }
+            if let placemark = placemarks?.first {
+                let street = placemark.thoroughfare ?? ""
+                let number = placemark.subThoroughfare ?? ""
+                let city = placemark.locality ?? ""
+                let address = "\(street) \(number), \(city)"
+                
+                DispatchQueue.main.async {
+                    self?.addressTextField.text = address
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self?.addressTextField.text = "Address not found"
+                }
+            }
+        }
+    }
 }
 
 extension UIView {
     func addSubviews(_ views: UIView...) {
         views.forEach { self.addSubview($0) }
+    }
+}
+
+extension OrderFooterView: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "DraggablePin"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.isDraggable = true
+            annotationView?.animatesDrop = true
+        } else {
+            annotationView?.annotation = annotation
+        }
+        return annotationView
     }
 }
